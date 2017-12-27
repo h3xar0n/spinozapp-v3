a2aedb50b7e932d3d597cec59ccb57ba13092c39
Aron Eidelman <aron@eideldev.com>
2017-12-27 13:52:28 -0800

    Attempting graph route

---
+++ src/components/home/Home.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>This is where the Homepage will go</h1>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import Graph from '@/components/graph/Graph'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/graph',
+      name: 'Graph',
+      component: Graph
     }
   ]
 })
