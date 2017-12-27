42e3eca2ce226dfccbf6d0082597bb383c3b0d94
Aron Eidelman <aron@eideldev.com>
2017-12-27 14:04:35 -0800

    History mode to elim annoying hash

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
     <b-navbar toggleable="md" type="dark" variant="info" class="main-nav">
         <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
-        <b-navbar-brand href="#">
+        <b-navbar-brand to="/">
             <img src="../assets/spinoza-menu.png" alt="" class="spinoza-img">
             spinozapp
         </b-navbar-brand>
@@ -11,17 +11,17 @@
             <!-- Right aligned nav items -->
             <b-navbar-nav class="ml-auto">
                 <b-button-group>
-                    <b-button>Resolution</b-button>
+                    <b-button to="/resolution">Resolution</b-button>
                     <b-dropdown right text="Reader">
-                        <b-dropdown-item>Book 1</b-dropdown-item>
-                        <b-dropdown-item>Book 2</b-dropdown-item>
-                        <b-dropdown-item>Book 3</b-dropdown-item>
-                        <b-dropdown-item>Book 4</b-dropdown-item>
-                        <b-dropdown-item>Book 5</b-dropdown-item>
+                        <b-dropdown-item to="/reader">Book 1</b-dropdown-item>
+                        <b-dropdown-item to="/">Book 2</b-dropdown-item>
+                        <b-dropdown-item to="/">Book 3</b-dropdown-item>
+                        <b-dropdown-item to="/">Book 4</b-dropdown-item>
+                        <b-dropdown-item to="/">Book 5</b-dropdown-item>
                         <b-dropdown-divider></b-dropdown-divider>
-                        <b-dropdown-item>Appendix</b-dropdown-item>
+                        <b-dropdown-item to="/">Appendix</b-dropdown-item>
                     </b-dropdown>
-                    <b-button>Graph</b-button>
+                    <b-button to="/graph">Graph</b-button>
                     
                 </b-button-group>
             </b-navbar-nav>
--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,29 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/home/Home'
+import Resolution from '@/components/resolution/Resolution'
+import Reader from '@/components/reader/Reader'
 import Graph from '@/components/graph/Graph'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'Home',
       component: Home
+    },
+    {
+      path: '/resolution',
+      name: 'Resolution',
+      component: Resolution
+    },
+    {
+      path: '/reader',
+      name: 'Reader',
+      component: Reader
     },
     {
       path: '/graph',
