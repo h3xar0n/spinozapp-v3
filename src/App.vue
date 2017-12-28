32e5a4961c2b026b57cfc0e58ac89594a744e891
Aron Eidelman <aron@eideldev.com>
2017-12-27 21:15:32 -0800

    Style nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
     <app-header></app-header>
-    <img src="./assets/logo.png">
     <router-view></router-view>
   </div>
 </template>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-    <b-navbar toggleable="md" type="dark" variant="info" class="main-nav">
+    <b-navbar toggleable="md" type="dark" class="main-nav">
         <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
         <b-navbar-brand to="/">
             <img src="../assets/spinoza-menu.png" alt="" class="spinoza-img">
@@ -11,15 +11,7 @@
             <!-- Right aligned nav items -->
             <b-navbar-nav class="ml-auto">
                     <b-button to="/resolution">Resolution</b-button>
-                    <b-dropdown right text="Reader">
-                        <b-dropdown-item to="/reader">Book 1</b-dropdown-item>
-                        <b-dropdown-item to="/">Book 2</b-dropdown-item>
-                        <b-dropdown-item to="/">Book 3</b-dropdown-item>
-                        <b-dropdown-item to="/">Book 4</b-dropdown-item>
-                        <b-dropdown-item to="/">Book 5</b-dropdown-item>
-                        <b-dropdown-divider></b-dropdown-divider>
-                        <b-dropdown-item to="/">Appendix</b-dropdown-item>
-                    </b-dropdown>
+                    <b-button to="/reader">Reader</b-button>
                     <b-button to="/graph">Graph</b-button>
                     
             </b-navbar-nav>
@@ -42,8 +34,15 @@ a.navbar-brand {
     top: 0;
     width: 100%
 }
-nav.navbar.main-nav.navbar-dark.bg-info.navbar-expand-md {
+.btn-secondary {
+    margin: 10px;
+}
+.btn-secondary:not([disabled]):not(.disabled).active {
+    background-color: #41b883
+}
+nav.navbar.main-nav.navbar-dark.navbar-expand-md {
     padding: 0;
     margin: 0;
+    background: #35495e
 }
 </style>
