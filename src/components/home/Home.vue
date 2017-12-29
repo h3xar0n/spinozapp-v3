be695d9d2221a65829b0e9a03a5ebb7ae567889d
Aron Eidelman <aron@eideldev.com>
2017-12-28 18:32:46 -0800

    Video and landing info

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -1,3 +1,24 @@
 <template>
-    <h1>This is where the Homepage will go</h1>
+    <div>
+        <div>
+            <b-embed type="iframe"
+                    aspect="16by9"
+                    src="https://www.youtube.com/embed/pVEeXjPiw54"
+                    allowfullscreen
+            ></b-embed>
+        </div>
+        <b-container class="bv-example-row">
+            <b-row>
+                <b-col>1 of 3</b-col>
+                <b-col>2 of 3</b-col>
+                <b-col>3 of 3</b-col>
+            </b-row>
+        </b-container>
+        <!-- section overview with routing -->
+        <!-- team section w/ cool background -->
+        <!-- contribution section with images -->
+        <!-- external videos -->
+        <!-- external links -->
+        <!-- contact form -->
+    </div>
 </template>
