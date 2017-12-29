342a312d5bd198ae8ad6862fb8d40ceba18c41ff
Aron Eidelman <aron@eideldev.com>
2017-12-29 15:57:25 -0800

    Basics covered

--- src/components/affects/Affects.vue
+++ src/components/affects/Affects.vue
@@ -30,9 +30,6 @@
               <p class="card-text">
               Some quick example text to build on the card title and make up the bulk of the card's content.
               </p>
-              <b-button href="#" variant="primary">Find a resolution</b-button>
-              <br>
-              <br>
               <b-button to="/reader" variant="primary">Read in the Ethics</b-button>
             </b-card>
           </div>
@@ -78,7 +75,7 @@
               <p class="card-text">
               Some quick example text to build on the card title and make up the bulk of the card's content.
               </p>
-              <b-button href="#" variant="primary">Find a resolution</b-button>
+              <b-button @click="selectedAffects.love = true" variant="primary">Find a resolution</b-button>
               <br>
               <br>
               <b-button to="/reader" variant="primary">Read in the Ethics</b-button>
@@ -95,8 +92,7 @@
       </div>
       <!-- affect checkbox and resolutions -->
       <div class="row">
-        <div class="col-xs-12 col-md-8"></div>
-        <div class="col-xs-12 col-md-4">
+        <div class="col-md-3">
           <ul>
             <li>
               <input type="checkbox" id="joy" value="true" v-model="selectedAffects.joy">
@@ -118,6 +114,33 @@
           <br>
           <span>Checked names: {{ selectedAffects }}</span>
         </div>
+        <div class="col-md-4" v-if="selectedAffects.sorrow === true">
+          <b-card title="Solution for sorrow"
+              img-alt="Image"
+              img-top
+              tag="article"
+              class="mb-2">
+            <img src="../../assets/content.png" alt="" style="max-width: 100%;">
+            <p class="card-text">
+            Some quick example text to build on the card title and make up the bulk of the card's content.
+            </p>
+            <b-button to="/reader" variant="primary">Read in the Ethics</b-button>
+          </b-card>
+        </div>
+        <div class="col-md-4" v-if="selectedAffects.love === true">
+          <b-card title="Refinement for love"
+              img-alt="Image"
+              img-top
+              tag="article"
+              class="mb-2">
+            <img src="../../assets/content.png" alt="" style="max-width: 100%;">
+            <p class="card-text">
+            Some quick example text to build on the card title and make up the bulk of the card's content.
+            </p>
+            <b-button to="/reader" variant="primary">Read in the Ethics</b-button>
+          </b-card>
+        </div>
+        
       </div>
     </div>
   </div>
