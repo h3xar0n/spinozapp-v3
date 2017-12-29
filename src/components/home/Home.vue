5669a503ad135cf19fe82f098ff4e1ac5c06d502
Aron Eidelman <aron@eideldev.com>
2017-12-28 19:37:40 -0800

    Tracking graph for fundraising goals

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -69,7 +69,9 @@
                     <img src="../../assets/spinoza-money.jpg" alt="">
                     
                     <h2>Cash</h2>
-                    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                    <b-progress :value="70" class="w-75 mb-2"></b-progress>
+                    <b-progress :value="13" class="w-50 mb-2"></b-progress>
+                    <b-progress :value="3" class="w-25"></b-progress>
                 </b-col>
             </b-row>
         </b-container>
