de8d1cee0d97c97b670ba83eecd610eeaae95bea
Aron Eidelman <aron@eideldev.com>
2017-12-28 19:53:57 -0800

    Features component

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -1,27 +1,7 @@
 <template>
     <div>
         <jumbotron />
-        <b-container class="features-info">
-            <b-row>
-                <b-col>
-                    <h2>Affects</h2>
-                    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-                    <b-button to="/affects">Affects</b-button>
-                </b-col>
-                <b-col>
-                    <h2>Reader</h2>
-                    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-                    <b-button to="/reader">Reader</b-button>        
-                </b-col>
-                <b-col>
-                    <h2>Graph</h2>
-                    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-                    <b-button to="/graph">Graph</b-button>
-                </b-col>
-            </b-row>
-        </b-container>
-        <br>
-        <br>
+        <features />
         <ul class="list-unstyled">
             <b-media tag="li">
                 <b-img slot="aside" blank blank-color="#abc" width="200" alt="placeholder" />
@@ -101,10 +81,12 @@
 
 <script>
 import Jumbotron from './Jumbotron.vue'
+import Features from './Features.vue'
 
 export default {
   components: {
-    jumbotron: Jumbotron
+    jumbotron: Jumbotron,
+    features: Features
   }
 }
 </script>
