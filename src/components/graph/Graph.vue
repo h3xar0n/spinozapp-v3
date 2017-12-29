c87b0b5f9697a28afc9df58f007b2b326952f52d
Aron Eidelman <aron@eideldev.com>
2017-12-28 21:50:43 -0800

    Remove unused script

--- src/components/graph/Graph.vue
+++ src/components/graph/Graph.vue
@@ -3,8 +3,3 @@
         <iframe src="http://ethics.spinozism.org/graphs/planar5.php" style="border:0px #ffffff none;" name="myiFrame" scrolling="no" frameborder="1" marginheight="0px" marginwidth="0px" height="2500px" width="1000px" allowfullscreen></iframe>
     </div>
 </template>
-
-<script>
-    export default {
-    }
-</script>
