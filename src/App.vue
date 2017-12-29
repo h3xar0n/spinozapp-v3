abf3bd0cc542a86305a5525348ffaa980afd149a
Aron Eidelman <aron@eideldev.com>
2017-12-28 21:42:28 -0800

    Add footer

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <app-header></app-header>
-    <router-view class="view"></router-view>
+    <app-header />
+    <router-view class="view" />
+    <app-footer/>  
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
 
 export default {
   name: 'app',
   components: {
-    appHeader: Header
+    appHeader: Header,
+    appFooter: Footer
   }
 }
 </script>
@@ -22,10 +25,10 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  padding-top: 100px;
-
+  padding-top: 65px;
   background-color: #fdf3f3;
 }
-
-
+.view {
+  padding-bottom: 100px
+}
 </style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <footer>
+      <p>
+          ☮ 2017-2018 Aron Eidelman
+      </p>
+  </footer>
+</template>
+
+<style>
+    footer {
+        color: white;
+        background: #35495e;
+        height: 100px;
+        padding: 40px;
+    }
+    footer p {
+        text-align: left;
+    }
+</style>
