f62cbfe2caf68f7dc18b01502f24db27ec133a98
Aron Eidelman <aron@eideldev.com>
2017-12-29 19:20:13 -0800

    Create dummy components for each book

---
+++ src/components/reader/Book5.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Book 5</h1>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,8 @@ import Router from 'vue-router'
 import Home from '@/components/home/Home'
 import Affects from '@/components/affects/Affects'
 import Reader from '@/components/reader/Reader'
+import Book1 from '@/components/reader/Book1'
+import Book2 from '@/components/reader/Book2'
 import Graph from '@/components/graph/Graph'
 
 Vue.use(Router)
@@ -23,7 +25,17 @@ export default new Router({
     {
       path: '/reader',
       name: 'Reader',
-      component: Reader
+      component: Reader,
+      children: [
+        {
+          path: 'book1',
+          component: Book1
+        },
+        {
+          path: 'book2',
+          component: Book2
+        }
+      ]
     },
     {
       path: '/graph',
