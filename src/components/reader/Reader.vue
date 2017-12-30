7578edc8bbfbd6c30492f7fd701f110e7d0f7f62
Aron Eidelman <aron@eideldev.com>
2017-12-29 18:49:09 -0800

    Indentation

--- src/components/reader/Reader.vue
+++ src/components/reader/Reader.vue
@@ -1,29 +1,30 @@
 <template>
-    <div class="row approach">
-        <div class="col-xs-3 col-md-3">
-            <h2>Dependencies</h2>
-        </div>
-        <div class="col-xs-6 col-md-6 detail-card">
-            <b-card title="Card Title"
-                    img-src="https://lorempixel.com/600/300/food/5/"
-                    img-alt="Image"
-                    img-top
-                    tag="article"
-                    class="mb-2">
-                <p class="card-text">
-                Some quick example text to build on the card title and make up the bulk of the card's content.
-                </p>
-                <b-button href="#" variant="primary">Go somewhere</b-button>
-            </b-card>
-        </div>
-        <div class="col-xs-3 col-md-3">
-            <h2>Involvements</h2>
-        </div>
+  <div class="approach">
+    <div class="row">
+      <h1></h1>
     </div>
+    <div class="row">
+      <div class="col-xs-3 col-md-3">
+        <h2>Dependencies</h2>
+      </div>
+      <div class="col-xs-6 col-md-6 detail-card">
+        <b-card title="Card Title" img-src="https://lorempixel.com/600/300/food/5/" img-alt="Image" img-top tag="article" class="mb-2">
+          <p class="card-text">
+            Some quick example text to build on the card title and make up the bulk of the card's content.
+          </p>
+          <b-button href="#" variant="primary">Go somewhere</b-button>
+        </b-card>
+      </div>
+      <div class="col-xs-3 col-md-3">
+        <h2>Involvements</h2>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style>
-    .card {
-        width: 100%
-    }
+  .card {
+    width: 100%
+  }
+
 </style>
