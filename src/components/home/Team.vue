5721dee077cc829ec333d00331a16c420b40dead
Aron Eidelman <aron@eideldev.com>
2017-12-31 15:09:44 -0800

    Style team section

--- src/components/home/Team.vue
+++ src/components/home/Team.vue
@@ -1,25 +1,26 @@
 <template>
   <ul class="list-unstyled">
     <b-media tag="li">
-      <b-img slot="aside" blank blank-color="#abc" width="200" alt="placeholder" />
-      <h5 class="mt-0 mb-1">Aroshka</h5>
-      Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in
-      vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis
-      in faucibus.
-    </b-media>
-    <b-media tag="li" class="my-4">
       <b-img slot="aside" blank blank-color="#cba" width="200" alt="placeholder" />
-      <h5 class="mt-0 mb-1">Adelinchik</h5>
-      Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in
-      vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis
-      in faucibus.
+      <h5 class="mt-0 mb-1">Adelina</h5>
+      <p>A makeup artist and hairstylist with a penchant for UI design, color theory, and painting, Adelina
+        guides the visual design choices on Spinozapp. Her philosophical interests include the distinct conceptions of virtue
+        in Spinoza and Machiavelli, as well as how to view works of art <i>sub specie aeternitatas</i> (under the aspect of eternity).
+      </p>
     </b-media>
     <b-media tag="li">
-      <b-img slot="aside" blank blank-color="#bac" width="200" alt="placeholder" />
-      <h5 class="mt-0 mb-1">Spinoza</h5>
-      Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in
-      vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis
-      in faucibus.
+      <b-img slot="aside" blank blank-color="#abc" width="200" alt="placeholder" />
+      <h5 class="mt-0 mb-1">Aron</h5>
+      <p>A software engineer with a lifelong devotion to philosophy, Aron sees the potential of life within lines of code.
+        He leads development, system design, and content for Spinozapp. His interests include the application of
+        Spinoza's ontology to computer science, and how technology can help people foster ethical reasoning skills.
+      </p>
     </b-media>
   </ul>
 </template>
+
+<style>
+  .media {
+    padding: 40px 80px 0 80px;
+  }
+</style>
