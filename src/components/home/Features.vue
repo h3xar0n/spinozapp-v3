e423a020bdb0023c2ac56d707a64a963dc0e2d66
Aron Eidelman <aron@eideldev.com>
2017-12-31 15:24:37 -0800

    Format text for features

--- src/components/home/Features.vue
+++ src/components/home/Features.vue
@@ -4,7 +4,11 @@
       <b-col sm="12" md="4">
         <h1>⚡</h1>
         <h2>Affects</h2>
-        <p>Customize your reading experience by focusing on the affects that pertain the most to you.</p>
+        <p>Customize your reading 
+          experience by focusing 
+          on the affects that 
+          pertain the most to you.</p>
+        <br>
         <br>
         <br>
         <b-button to="/affects">Affects</b-button>
@@ -15,12 +19,14 @@
         <p>Read each element in the Ethics with its dependencies and involvements in sight at all times</p>
         <br>
         <br>
+        <br>
         <b-button to="/reader">Reader</b-button>
       </b-col>
       <b-col sm="12" md="4">
         <h1>🌐</h1>
         <h2>Graph</h2>
         <p>Navigate the Ethics with an interactive visualization of the connections between the elements</p>
+        <br>
         <br>
         <br>
         <b-button to="/graph">Graph</b-button>
@@ -33,11 +39,19 @@
   .features-info {
     max-width: 100vw;
     padding-bottom: 40px;
+    padding-left: 10%;
+    padding-right: 10%;
     position: relative;
   }
 
   .features-info .col-sm-12 {
     padding-top: 40px;
+  }
+
+  .features-info p {
+    text-align: left;
+    widows: 4;
+    margin: 0 auto;
   }
 
   .features-info a {
