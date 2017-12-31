80a8ca75a43c63d041c9b65dc94e0c5fd6911489
Aron Eidelman <aron@eideldev.com>
2017-12-31 14:25:01 -0800

    Construction notice for all books

--- src/components/reader/Book5.vue
+++ src/components/reader/Book5.vue
@@ -1,3 +1,16 @@
 <template>
-  <h1>Book 5</h1>
+  <div>
+    <h1>Book 5</h1>
+    <under-construction></under-construction>
+  </div>
 </template>
+
+<script>
+  import Construction from '../Construction.vue'
+
+  export default {
+    components: {
+      underConstruction: Construction
+    }
+  }
+</script>
