2db472a1d7b1378c767f46fb6e0ee4907f850cb6
Aron Eidelman <aron@eideldev.com>
2017-12-31 19:28:10 -0800

    Styling contribution section

--- src/components/home/Contribution.vue
+++ src/components/home/Contribution.vue
@@ -5,43 +5,54 @@
         <b-col sm="12" md="6">
           <img src="../../assets/gitlab-logo.png" alt="">
           <h3 class="subtitle">Development</h3>
-          <p>Whether it's refactoring our components or adding new features, there is always more work to be done behind
-            the scenes. Check out our <a href="https://gitlab.com/eideldev/spinozapp/milestones" target="_blank">issues</a>, 
-            fork our <a href="https://gitlab.com/eideldev/spinozapp" target="_blank">repo</a>, and <a href="mailto:aron@eideldev.com">
-              contact us</a> if you have new ideas!</p>
+          <p>Whether it's refactoring our components or adding new features, there is always more work to be done behind the
+            scenes. Check out our
+            <a href="https://gitlab.com/eideldev/spinozapp/milestones" target="_blank">issues</a>, fork our
+            <a href="https://gitlab.com/eideldev/spinozapp" target="_blank">repo</a>, and
+            <a href="mailto:aron@eideldev.com">
+              contact us</a> if you have new ideas! These are our main tools:</p>
           <div class="code-tools">
-              <div>
+            <div>
+              <a href="https://vuejs.org/" target="_blank">
                 <i class="devicon-vuejs-plain colored"></i>
-                <p>Vue</p>  
-              </div>
-              <div>
+                <p>Vue</p>
+              </a>
+            </div>
+            <div>
+              <a href="https://d3js.org/" target="_blank">
                 <i class="devicon-d3js-plain colored"></i>
                 <p>D3</p>
-              </div>
-              <div>
+              </a>
+            </div>
+            <div>
+              <a href="http://sass-lang.com/" target="_blank">
                 <i class="devicon-sass-original colored"></i>
                 <p>Sass</p>
-              </div>
+              </a>
+            </div>
           </div>
         </b-col>
         <b-col sm="12" md="6">
           <img src="../../assets/spinoza-money.png" alt="">
           <h3 class="subtitle">Donations</h3>
           <p>
-            Our goal is to get a custom domain name with one email address and HTTPS protection. 
-            This will allow us to add new features, such as creating user profiles. 
-            Any excess proceeds will go to the <a href="https://www.chp-sf.org/about/" target="_blank">Community Housing Partnership</a>.
+            Our goal is to get a custom domain, an email address, and HTTPS. We're also aiming to use Heroku to serve an API. This will
+            allow us to add new features, such as user profiles, comments, and search. If we exceed our goal, proceeds will
+            go to the
+            <a href="https://www.chp-sf.org/about/" target="_blank">Community Housing Partnership</a>.
           </p>
           <div class="goals">
-            <span>Hosting plan</span>
+            <span>PaaS for API</span>
+            <b-progress :value="20" class="w-100" />
+            <span>Custom domain</span>
             <b-progress :value="20" class="w-75" />
-            <span>Custom domain with HTTPS</span>
+            <span>HTTPS</span>
             <b-progress :value="13" class="w-50" />
             <span>Email</span>
             <b-progress :value="3" class="w-25" />
           </div>
           <br>
-          
+
         </b-col>
       </b-row>
     </b-container>
@@ -85,6 +96,10 @@
 
   .code-tools div {
     display: inline-block;
+  }
+
+  .code-tools div a:hover {
+    text-decoration: none;
   }
 
   .code-tools div:nth-child(2) {
