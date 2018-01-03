7dfe8b815ece717edc08a29a96e326093c188146
Aron Eidelman <aron@eideldev.com>
2018-01-02 23:22:10 -0800

    Factor out bootstrap nav

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,45 +1,31 @@
 <template>
-  <b-navbar toggleable="md" type="dark" class="main-nav">
-    <b-navbar-brand to="/">
-      <img src="../assets/spinoza-menu.png" alt="" class="spinoza-img"> spinozapp
-    </b-navbar-brand>
-    <div class="button-group">
-      <b-button to="/affects">Affects</b-button>
-      <b-button to="/reader">Reader</b-button>
-      <b-button to="/graph">Graph</b-button>
+  <nav class="main-nav">
+    <router-link to="/" class="nav-logo">
+      <img src="../assets/spinoza-menu.png" alt="" class="spinoza-img"> 
+      <span class="app-title">spinozapp</span>
+    </router-link>
+    <div class="nav-buttons">
+      <router-link class="button" to="/affects">Affects</router-link>
+      <router-link class="button" to="/reader">Reader</router-link>
+      <router-link class="button" to="/graph">Graph</router-link>
     </div>
-  </b-navbar>
+    <b-collapse is-nav id="nav_collapse">
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <b-button to="/graph">Graph</b-button>
+      </b-navbar-nav>
+    </b-collapse>
+  </nav>
+
 </template>
 
-<style>
-  .main-nav {
-    display: flex;
-    justify-content: space-between!important;
-  }
-
-  .spinoza-img {
-    height: 60px;
-    width: auto;
-  }
-
-  a.navbar-brand, a.navbar-brand:focus {
-    padding-bottom: 0;
-    color: black;
-  }
-
-  a.navbar-brand:hover {
-    color: #f6be0b;
-    background-color: none;
-    transition: 0.2s;
-  }
-
+<style scoped>
   .main-nav {
     position: fixed;
+    display: flex;
+    justify-content: space-between;
     top: 0;
-    width: 100%
-  }
-
-  nav.navbar.main-nav.navbar-dark.navbar-expand-md {
+    width: 100%;
     z-index: 3;
     padding: 0;
     margin: 0;
@@ -47,16 +33,65 @@
     box-shadow: 2px 2px #f7f7f7;
   }
 
-  @media (max-width: 524px) {
-    .spinoza-img {
-      height: 30px;
-    }
-    a {
-      font-size: 10px;
-    }
-    .btn-secondary {
-      margin: 0px;
-    }
+  .spinoza-img {
+    height: 60px;
+    width: auto;
+  }
+  .nav-logo {
+    color: black;
+  }
+  .nav-logo:hover {
+    color: #f6be0b;
+    transition: 0.2s;
+    text-decoration: none;
+  }
+  .nav-buttons {
+    display: flex;
+    width: 300px;
+    align-items: center;
+    justify-content: space-between;
+  }
+  .button {
+    text-decoration: none;
+    background: none;
+    color: black;
+    border: 1px solid black;
+    border-radius: 83px;
+    width: 92px;
+    vertical-align: center;
+    font-size: 13px;
+    padding: 10px;
+    text-transform: uppercase;
+    background: none;
   }
 
+  .button:hover {
+    color: #f6be0b;
+    border-color: #f6be0b;
+    transition: 0.2s;
+  }
+
+  .button:active, .button:focus {
+    background-color: #f6be0b;
+    border: 1px solid #f6be0b;
+    color: white;
+    box-shadow: none;
+  }
+
+  @media (max-width: 544px) {
+    .spinoza-img {
+      height: 60px;
+      width: auto;
+    }
+    .app-title {
+      display: none;
+    }
+    .nav-buttons {
+      width: 220px;
+    }
+    .button {
+      width: 65px;
+      padding: 5px;
+    }
+  }
 </style>
