1289e6611249ed47784edf0667f8acb18fe70f38
h3xar0n <aron@eideldev.com>
2018-05-29 14:58:45 -0700

    Create generic content components

---
+++ src/components/affects/content/Hatred.vue
@@ -0,0 +1,13 @@
+<template>
+  <article>
+    <h1>Hatred</h1>
+    <blockquote>
+      Definition goes here
+    </blockquote>
+    <div>Video goes here</div>
+    <div>Solutions go here</div>
+    <br>
+    <h3>Variants:</h3>
+    <div>Variants go here</div>
+  </article>
+</template>
