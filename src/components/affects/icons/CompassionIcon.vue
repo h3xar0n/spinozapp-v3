56c2e37bf859f5c09c9bf83b0cace74c8a3b173f
h3xar0n <aron@eideldev.com>
2018-05-29 17:06:28 -0700

    Do something generic for each unfilled icon component

---
+++ src/components/affects/icons/CompassionIcon.vue
@@ -0,0 +1,12 @@
+<template>
+  <router-link to="/affects/">
+    <div class="icon-component">
+      <i class="fas fa-bolt"></i>
+      <p>
+        Affect
+        <br>
+        <em>Affectus</em>
+      </p>          
+    </div>
+  </router-link>
+</template>
--- src/components/reader/Navigation.vue
+++ src/components/reader/Navigation.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="book-nav">
-    <b-button to='/reader/book1'>Book 1</b-button>
-    <b-button to='/reader/book2'>Book 2</b-button>
-    <b-button to='/reader/book3'>Book 3</b-button>
-    <b-button to='/reader/book4'>Book 4</b-button>
-    <b-button to='/reader/book5'>Book 5</b-button>
+    <router-link to='/reader/book1'>Book 1</router-link>
+    <router-link to='/reader/book2'>Book 2</router-link>
+    <router-link to='/reader/book3'>Book 3</router-link>
+    <router-link to='/reader/book4'>Book 4</router-link>
+    <router-link to='/reader/book5'>Book 5</router-link>
   </div>
 </template>
 
