b1fc7b9810a6002e9054db041221f0456db9326e
h3xar0n <you@example.com>
2018-06-02 13:20:34 -0700

    Split solutions state into different file

---
+++ src/store/solutions.js
@@ -0,0 +1,26 @@
+export const solutions = {
+  1: {
+    favorite: true,
+      text: 'We can re-order the ideas that we have'
+  },
+  2: {
+    favorite: false,
+      text: 'Here we go with v-if'
+  },
+  3: {
+    favorite: false,
+      text: 'Testing'
+  },
+  4: {
+    favorite: false,
+      text: 'A different affect'
+  },
+  5: {
+    favorite: true,
+    text: 'awesome.'
+  },
+  // #: {
+  //   favorite: false,
+  //   text: ''
+  // },
+}
--- src/store/store.js
+++ src/store/store.js
@@ -1,29 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { solutions } from './solutions'
 
 Vue.use(Vuex)
 
 const state = {
   affects: {
-  },
-  solutions: {
-    1: {
-      favorite: true,
-      text: 'We can re-order the ideas that we have'
-    },
-    2: {
-      favorite: false,
-      text: 'Here we go with v-if'
-    },
-    3: {
-      favorite: false,
-      text: 'Testing'
-    },
-    4: {
-      favorite: false,
-      text: 'A different affect'
-    }
-  }
+  }, 
+  solutions
 }
 
 export default new Vuex.Store({
