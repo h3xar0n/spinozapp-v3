4b9b03be4f37481d4ad589c63d4741a840ca741b
h3xar0n <aron@eideldev.com>
2018-06-02 15:11:35 -0700

    Testing out solutions state on PC

--- src/store/solutions.js
+++ src/store/solutions.js
@@ -1,23 +1,23 @@
 export const solutions = {
   1: {
-    favorite: true,
-      text: 'We can re-order the ideas that we have'
+    favorite: false,
+    text: 'This is one solution for affect'
   },
   2: {
     favorite: false,
-      text: 'Here we go with v-if'
+    text: 'This is another solution for affect'
   },
   3: {
     favorite: false,
-      text: 'Testing'
+    text: 'Another affect solution'
   },
   4: {
     favorite: false,
-      text: 'A different affect'
+    text: 'This one is for action'
   },
   5: {
-    favorite: true,
-    text: 'awesome.'
+    favorite: false,
+    text: 'another action'
   }
   // #: {
   //   favorite: false,
